8be39c199cc170b44fea0cf29c7d981f715a3b63
Pol Reig <pol@zizeron.tech>
2023-05-23 15:03:03 +0200

    added metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,8 +1,29 @@
+import { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 
 import './globals.css';
 
+
 import { i18n } from '../../i18n-config';
+
+export const metadata: Metadata = {
+  title: `Pol's Portfolio`,
+  description: 'This is my portfolio',
+  openGraph: {
+    title: `Pol's Portfolio`,
+    description: 'This is my portfolio',
+    url: 'https://www.polreigbroto.com',
+    siteName: `Pol's Portfolio`,
+    images: [
+      {
+        url: 'https://www.polreigbroto.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fte1jbfhy%2Fproduction%2F9b3888002eb7477a104d7c2fdde0f3d2b5897db2-2937x2937.png&w=828&q=75',
+        alt: 'Profile Image',
+        width: 600,
+        height: 600,
+      },
+    ],
+  },
+};
 
 const montserrat = Montserrat({
   weight: ['400', '700'],
